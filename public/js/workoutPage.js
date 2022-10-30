2f457dfd84b2943818eff50eabfec9929a555d45
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-10-29 21:13:42 -0500

    changed timer on workout page

--- public/js/workoutPage.js
+++ public/js/workoutPage.js
@@ -1,247 +1,121 @@
-//progress bars overlay
-const radius = 90,
-  padding = 50,
-  radians = 2 * Math.PI;
-
-const dimension = 2 * radius + 2 * padding,
-  points = 50,
-  percentage = 0.62;
-
-const angle = d3.scale
-  .linear()
-  .domain([0, points - 1])
-  .range([0, radians]);
-
-const line = d3.svg.line
-  .radial()
-  .interpolate("basis")
-  .tension(0)
-  .radius(radius)
-  .angle((d, i) => {
-    if (i < points * percentage + 1) {
-      return angle(i);
-    }
-  });
-
-const svg = d3
-  .select(".outer-circle")
-  .append("svg")
-  .attr("width", dimension)
-  .attr("height", 200)
-  .append("g");
-
-svg
-  .append("path")
-  .datum(d3.range(points))
-  .attr("class", "line")
-  .attr("fill", "none")
-  .attr("stroke-dasharray", "7 3")
-  .attr("stroke-width", "25px")
-  .attr("stroke", "#1F2025") //#1F2025
-  .attr("d", line)
-  .attr("transform", "translate(135,106) rotate(-110)");
-
-//progress bars
-let elapsedPercent = 0;
-const r = 100;
-const pi = Math.PI;
-const green1 = "#B8D087";
-const green2 = "#00996D";
-const data = {
-  upper: calcPercent(0),
-  lower: calcPercent(elapsedPercent),
-};
-let progress = 0;
-
-function calcPercent(percent) {
-  return [percent, 100 - percent];
-}
-
-const canvas = d3
-  .select(".inner-circle")
-  .append("svg")
-  .attr("height", r + 100)
-  .attr("width", r + 150);
-
-const group = canvas
-  .append("g")
-  .attr("transform", "translate(" + 120 + "," + 110 + ")");
-
-const arc = d3.svg
-  .arc()
-  .innerRadius(r / 1.2)
-  .outerRadius(r);
-
-//returns objects based on data
-const pie = d3.layout
-  .pie()
-  .sort(null)
-  .value((data) => data)
-  .startAngle(-110 * (pi / 180))
-  .endAngle(110 * (pi / 180));
-
-const arcs = group
-  .selectAll(".arc")
-  .data(pie(data.lower))
-  .enter()
-  .append("g")
-  .attr("class", "arc");
-
-const defs = canvas
-  .append("defs")
-  .append("linearGradient")
-  .attr("id", "greenGradient")
-  .attr("gradientUnits", "objectBoundingBox")
-  .attr("x1", "0")
-  .attr("y1", "0")
-  .attr("x2", "1")
-  .attr("y2", "1");
-
-defs.append("stop").attr("offset", "0%").attr("stop-color", green1);
-defs.append("stop").attr("offset", "100%").attr("stop-color", green2);
-
-let path = arcs
-  .append("path")
-  .attr("class", (data, index) => "progress-color" + index)
-  .attr("d", arc);
-
-/////////timer
-const startButton = document.getElementById("start-timer");
-const resetButton = document.getElementById("reset-timer");
-const timeDisplay = document.getElementById("timer-display-time");
-const statusDisplay = document.getElementById("status");
-const spinner = document.getElementById("spineroo");
-let sessionTimer = parseInt(
-  document.getElementById("set-timer-display").innerHTML,
-  10
-);
-let breakTimer = parseInt(
-  document.getElementById("set-break-display").innerHTML,
-  10
-);
-elapsedPercent = 0;
-let seconds = 60;
-let minutes;
-let timer;
-let breakTime = false;
-
-//start timer
-startButton.addEventListener("click", () => {
-  timer = setInterval(timerFn, 1000);
-  //console.log("button: " + startButton.innerHTML.trim() + " | session: " + sessionTimer + " | break: " + breakTimer);
-  startButton.classList.toggle("hidden");
-  resetButton.classList.toggle("hidden");
-  spinner.classList.toggle("spinning");
-  statusDisplay.innerHTML = "In session!";
-  minutes = sessionTimer - 1;
-  function timerFn() {
-    seconds--;
-    //console.log("time: " +minutes + ":" + seconds + " | percent: " + elapsedPercent);
-    progress();
-    if (seconds < 10 && minutes < 10) {
-      timeDisplay.innerHTML = "0" + minutes + ":0" + seconds;
-    } else if (seconds < 10) {
-      timeDisplay.innerHTML = minutes + ":0" + seconds;
-    } else if (minutes < 10) {
-      timeDisplay.innerHTML = "0" + minutes + ":" + seconds;
-    } else {
-      timeDisplay.innerHTML = minutes + ":" + seconds;
-    }
-    if (seconds === 0 && minutes === 0) {
-      if (breakTime === false) {
-        breakTime = true;
-        minutes = breakTimer - 1;
-        seconds = 60;
-        elapsedPercent = 0;
-        statusDisplay.innerHTML = "Take a break!";
-        console.log("break time");
-      } else {
-        breakTime = false;
-        minutes = sessionTimer - 1;
-        seconds = 60;
-        elapsedPercent = 0;
-        statusDisplay.innerHTML = "In session!";
-        console.log("work time");
-      }
-    }
-    if (seconds === 0) {
-      minutes--;
-      seconds = 60;
-    }
-  }
-  function progress() {
-    let secondsElapsed = minutes * 60 + seconds;
-    let totalSeconds;
-    if (breakTime === true) {
-      totalSeconds = breakTimer * 60;
-    } else {
-      totalSeconds = sessionTimer * 60;
-    }
-    elapsedPercent = (1 - secondsElapsed / totalSeconds) * 100;
-    path.data(pie(calcPercent(elapsedPercent))).attr("d", arc);
-  }
-});
-
-//reset timer
-resetButton.addEventListener("click", () => {
-  startButton.classList.toggle("hidden");
-  resetButton.classList.toggle("hidden");
-  spinner.classList.toggle("spinning");
-  clearInterval(timer);
-  seconds = 60;
-  minutes = sessionTimer - 1;
-  elapsedPercent = 0;
-  timeDisplay.innerHTML = sessionTimer + ":00";
-  statusDisplay.innerHTML = "Reset!";
-  console.log("reset");
-  path.data(pie(calcPercent(elapsedPercent))).attr("d", arc);
-});
-
-//timer settings
-const settingOptions = {
-  "add-break": () => {
-    if (breakTimer < 51) {
-      let timer = breakTimer++;
-      document.getElementById("set-break-display").innerHTML = timer + 1;
-    }
-  },
-  "minus-break": () => {
-    if (breakTimer > 5) {
-      let timer = breakTimer--;
-      document.getElementById("set-break-display").innerHTML = timer - 1;
-    }
-  },
-  "add-timer": () => {
-    if (sessionTimer < 99) {
-      let timer = sessionTimer++;
-      document.getElementById("set-timer-display").innerHTML = timer + 1;
-      document.getElementById("timer-display-time").innerHTML =
-        timer + 1 + ":00";
-    }
-  },
-  "minus-timer": () => {
-    if (sessionTimer > 25) {
-      let timer = sessionTimer--;
-      document.getElementById("set-timer-display").innerHTML = timer - 1;
-      document.getElementById("timer-display-time").innerHTML =
-        timer - 1 + ":00";
-    }
-  },
-};
-
-const buttonSettings = document.getElementsByClassName("setting-button");
-
-for (let i = 0; i < buttonSettings.length; i++) {
-  buttonSettings[i].addEventListener("click", () => {
-    let operation = buttonSettings[i].getAttribute("id");
-    settingOptions[operation]();
-    startButton.classList.remove("hidden");
-    resetButton.classList.add("hidden");
-    spinner.classList.remove("spinning");
-    clearInterval(timer);
-    seconds = 60;
-    minutes = sessionTimer - 1;
-    elapsedPercent = 0;
-    console.log("setting up");
-  });
-}
+var pomodoroLengthMins = 25; // initial pomodoro length in mins  
+var origPomodoroLengthMins = pomodoroLengthMins; // copy to a new variable for later use  
+var pomodoroLengthSecs = (pomodoroLengthMins * 60) % 60; // initial pomodoro secs value  
+var pomodoroTotal = pomodoroLengthMins * 60; // total pomodoro length in secs  
+if (pomodoroLengthSecs < 10) {  
+ pomodoroLengthSecs = "0" + pomodoroLengthSecs; // output 0 secs as 00  
+}  
+var breakLengthMins = 5; // initial break length in mins  
+var origBreakLengthMins = breakLengthMins; // copy to a new variable for later use  
+var breakLengthSecs = (breakLengthMins * 60) % 60; // initial break secs value  
+var breakTotal = breakLengthMins * 60; // total break length in secs  
+if (breakLengthSecs < 10) {  
+ breakLengthSecs = "0" + breakLengthSecs; // output 0 secs as 00  
+}  
+var interval; // declare variable for use in the setInterval function  
+// display initial values of the timers on the page  
+document.querySelector('.pomodoro-timer').innerHTML = pomodoroLengthMins;  
+document.querySelector('.break-timer').innerHTML = breakLengthMins;  
+document.querySelector('.timer-main-container').innerHTML = pomodoroLengthMins + ":" + pomodoroLengthSecs;  
+// decrement break timer  
+function decrementBreakTimer() {  
+ if (breakLengthMins > 1) {  
+  breakLengthMins = breakLengthMins - 1;  
+  origBreakLengthMins = breakLengthMins;  
+  breakTotal = breakLengthMins * 60;  
+  document.querySelector('.break-timer').innerHTML = breakLengthMins;  
+ }  
+}  
+// increment break timer  
+function incrementBreakTimer() {  
+ breakLengthMins = breakLengthMins + 1;  
+ origBreakLengthMins = breakLengthMins;  
+ breakTotal = breakLengthMins * 60;  
+ document.querySelector('.break-timer').innerHTML = breakLengthMins;  
+}  
+// decrement Pomodoro timer  
+function decrementPomodoroTimer() {  
+ if (pomodoroLengthMins > 1) {  
+  pomodoroLengthMins = pomodoroLengthMins - 1;  
+  origPomodoroLengthMins = pomodoroLengthMins;  
+  pomodoroTotal = pomodoroLengthMins * 60;  
+  document.querySelector('.pomodoro-timer').innerHTML = pomodoroLengthMins;  
+  document.querySelector('.timer-main-container').innerHTML = pomodoroLengthMins + ":" + pomodoroLengthSecs;  
+ }  
+}  
+// increment Pomodoro timer  
+function incrementPomodoroTimer() {  
+ pomodoroLengthMins = pomodoroLengthMins + 1;  
+ origPomodoroLengthMins = pomodoroLengthMins;  
+ pomodoroTotal = pomodoroLengthMins * 60;  
+ document.querySelector('.pomodoro-timer').innerHTML = pomodoroLengthMins;  
+ document.querySelector('.timer-main-container').innerHTML = pomodoroLengthMins + ":" + pomodoroLengthSecs;  
+}  
+// start button: disable changing timer settings, and start the setInterval  
+function startTimer() {  
+  document.querySelector('.decrementBreakTimer').disabled = 'disabled';  
+  document.querySelector('.incrementBreakTimer').disabled = 'disabled';  
+  document.querySelector('.decrementPomodoroTimer').disabled = 'disabled';  
+  document.querySelector('.incrementPomodoroTimer').disabled = 'disabled';  
+  document.querySelector('.button-start').disabled = 'disabled';  
+  document.querySelector('.button-pause').disabled = '';  
+  interval = setInterval(runPomodoroTimer, 1000);  
+}  
+function runPomodoroTimer() {  
+ pomodoroTotal--; // decrement total pomodoro timer length  
+ pomodoroLengthMins = pomodoroTotal / 60 | 0; // calculate mins  
+ pomodoroLengthSecs = pomodoroTotal % 60; // calculate secs  
+ if (pomodoroLengthSecs < 10) { // output secs as two figures  
+  pomodoroLengthSecs = "0" + (pomodoroTotal % 60);  
+ }  
+ document.querySelector('.timer-main-container').innerHTML = pomodoroLengthMins + ":" + pomodoroLengthSecs;  
+ document.querySelector('.timer-description').innerHTML = "until your break";  
+ if (pomodoroTotal == -1) {  
+  clearInterval(interval);  
+  breakTotal = origBreakLengthMins * 60;  
+  document.querySelector('.timer-main-container').innerHTML = origBreakLengthMins + ":00";  
+  document.querySelector('.timer-description').innerHTML = "It's break time!";  
+  interval = setInterval(runBreakTimer, 1000);  
+ }  
+}  
+function runBreakTimer() {  
+ breakTotal--; // decrement counter  
+ breakLengthMins = breakTotal / 60 | 0; // calculate mins  
+ breakLengthSecs = breakTotal % 60; // calculate secs  
+ if (breakLengthSecs < 10) { // output secs as two figures  
+  breakLengthSecs = "0" + (breakTotal % 60);  
+ }  
+ document.querySelector('.timer-main-container').innerHTML = breakLengthMins + ":" + breakLengthSecs;  
+ if (breakTotal == -1) {  
+  breakTotal--;  
+  clearInterval(interval);  
+  pomodoroTotal = origPomodoroLengthMins * 60;  
+  document.querySelector('.timer-main-container').innerHTML = origPomodoroLengthMins + ":00";  
+  document.querySelector('.timer-description').innerHTML = "until your break";  
+  interval = setInterval(runPomodoroTimer, 1000);  
+ }  
+}  
+function pauseTimer() {  
+  clearInterval(interval);  
+  document.querySelector('.button-pause').disabled = 'disabled';  
+  document.querySelector('.button-start').disabled = '';  
+}  
+function resetTimer() {  
+ document.querySelector('.decrementBreakTimer').disabled = '';  
+ document.querySelector('.incrementBreakTimer').disabled = '';  
+ document.querySelector('.decrementPomodoroTimer').disabled = '';  
+ document.querySelector('.incrementPomodoroTimer').disabled = '';  
+ document.querySelector('.button-start').disabled = '';  
+ document.querySelector('.button-pause').disabled = '';  
+ clearInterval(interval);  
+ pomodoroLengthMins = 25;  
+ pomodoroLengthSecs = "0" + 10;  
+ pomodoroTotal = pomodoroLengthMins * 60;  
+ breakLengthMins = 5;  
+ breakLengthSecs = "0" + 0;  
+ breakTotal = breakLengthMins * 60;  
+ document.querySelector('.pomodoro-timer').innerHTML = pomodoroLengthMins;  
+ document.querySelector('.break-timer').innerHTML = breakLengthMins;  
+ document.querySelector('.timer-main-container').innerHTML = pomodoroLengthMins + ":" + pomodoroLengthSecs;  
+ document.querySelector('.timer-description').innerHTML = "Ready to work?";  
+}  
