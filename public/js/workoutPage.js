8a8f128d6a960f53665e31c93cf926aa0d51fc2f
kalina <kalina.boksa@gmail.com>
2022-10-31 19:09:30 -0500

    seed

--- public/js/workoutPage.js
+++ public/js/workoutPage.js
@@ -18,3 +18,34 @@ getExercicesData().then((data) => {
   }
   console.log("results:" + results);
 });
+
+// Timer
+document.getElementById("timer").exercisesData = 01 + ":" + 11;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById("timer").exercisesData;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond(timeArray[1] - 1);
+  if (s == 59) {
+    m = m - 1;
+  }
+  if (m < 0) {
+    return;
+  }
+
+  document.getElementById("timer").ExercisesData = m + ":" + s;
+  console.log(m);
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {
+    sec = "0" + sec;
+  } // add zero in front of numbers < 10
+  if (sec < 0) {
+    sec = "59";
+  }
+  return sec;
+}
