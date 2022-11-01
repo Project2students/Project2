756b5d8c4da6cc6c0ea4d2b388c715c0fd48928c
kalina <kalina.boksa@gmail.com>
2022-10-31 19:25:11 -0500

    detle local host

--- public/js/workoutPage.js
+++ public/js/workoutPage.js
@@ -2,9 +2,7 @@ const username = window.location.href.split("?")[1];
 const workoutList = document.querySelector("#workoutList");
 
 async function getExercicesData() {
-  const response = await fetch(
-    `http://localhost:3001/api/customWorkout/${username}`
-  );
+  const response = await fetch(`/api/customWorkout/${username}`);
   const exercisesData = await response.json();
   return exercisesData;
 }
@@ -18,34 +16,3 @@ getExercicesData().then((data) => {
   }
   console.log("results:" + results);
 });
-
-// Timer
-document.getElementById("timer").exercisesData = 01 + ":" + 11;
-startTimer();
-
-function startTimer() {
-  var presentTime = document.getElementById("timer").exercisesData;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond(timeArray[1] - 1);
-  if (s == 59) {
-    m = m - 1;
-  }
-  if (m < 0) {
-    return;
-  }
-
-  document.getElementById("timer").ExercisesData = m + ":" + s;
-  console.log(m);
-  setTimeout(startTimer, 1000);
-}
-
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {
-    sec = "0" + sec;
-  } // add zero in front of numbers < 10
-  if (sec < 0) {
-    sec = "59";
-  }
-  return sec;
-}
